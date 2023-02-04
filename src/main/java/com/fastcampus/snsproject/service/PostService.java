11910cff33e6572e546b8582ec2f0b90723941bb
DESKTOP-MIB955N\Hansu <hansu_kim@sk.com>
2023-02-04 17:11:30 +0900

    2023.02.04 Part8.Ch03-05 종료

--- src/main/java/com/fastcampus/snsproject/service/PostService.java
+++ src/main/java/com/fastcampus/snsproject/service/PostService.java
@@ -117,4 +117,9 @@ public class PostService {
 
         return likeEntityRepository.countByPost(postEntity);
     }
+
+    @Transactional
+    public void comment(Integer postId, String userName){
+
+    }
 }
